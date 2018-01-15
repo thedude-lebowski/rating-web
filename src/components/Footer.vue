2701f57023a274a60d3e4641234641ace4ca6d5c
Brian Redmond <brianisrunning@gmail.com>
2018-01-15 16:00:25 -0500

    test build

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Intelligent Cloud Blackbelt Team
+        Intelligent Cloud GBB Team
       </div>
       <div class="col-lg-6">
       </div>
