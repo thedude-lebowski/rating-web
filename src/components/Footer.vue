7691a08e02e9b0ea44d1dc947a3708051f67001c
Brian Redmond <brianisrunning@gmail.com>
2018-01-18 15:34:03 -0500

    updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Intelligent Cloud Blackbelt Team
+        Azure Global Blackbelt Team
       </div>
       <div class="col-lg-6">
       </div>
