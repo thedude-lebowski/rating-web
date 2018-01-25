3a33b477876023b0f8d6840bba37c766d4c86e78
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-01-25 08:16:15 -0500

    Fixed message in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Denver Hackfest
+        Linux Containers on Azure Workshop
       </div>
       <div class="col-lg-6">
       </div>
