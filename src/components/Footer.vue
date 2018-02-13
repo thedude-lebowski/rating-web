7b50e4017f62c5e4b76080bfc6767b7749f069a2
Brian Redmond <brianisrunning@gmail.com>
2018-02-13 16:59:25 -0500

    updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelts
+        Azure GBB Workshop Pittsburgh
       </div>
       <div class="col-lg-6">
       </div>
