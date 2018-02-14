44b1e3d3837a210a92be837b131a5ce2efaa1cf3
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-02-14 14:03:47 -0500

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure GBB Workshop Pittsburgh
+        Azure GBB Workshop Tampa
       </div>
       <div class="col-lg-6">
       </div>
