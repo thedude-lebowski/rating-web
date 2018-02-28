212cb05d40721215a874bc94a423aff84df396e3
Jeff Lebowski <36461087+thedude-lebowski@users.noreply.github.com>
2018-02-28 11:49:55 -0500

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure GBB Workshop Tampa
+        GBB Azure Container Hackfest
       </div>
       <div class="col-lg-6">
       </div>
